55fea637d07606091e42ba9efc779e999bf11329
Camila <mkp.camila@gmail.com>
2021-11-23 13:18:43 +0100

    add solution

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,20 +1,28 @@
 import "../styles/main.scss";
-import "babel-polyfill"
+import "babel-polyfill";
 
 const modal = document.getElementById("myModal");
-let promiseOfModal = new Promise(function (resolve) {
-    window.setTimeout(function () {
-        resolve(modal)
-    }, (1000 * 60));
-});
 
-promiseOfModal.then(function(val) {
-    console.log("User has been on the page for 60 seconds");
-    val.style.display = "block";
-})
 
-modal.addEventListener("click", (e) => {
-    switch(e.target.className) {
+function awaitMessage()
+{
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+            resolve(modal)
+        }, (1000*60));
+    })
+}
+async function displayBlock ()
+{
+    await awaitMessage();
+    modal.style.display = 'block'
+}
+displayBlock();
+
+modal.addEventListener("click", (e) => 
+{
+    switch(e.target.className) 
+    {
         case "close":
         case "modal":
         modal.style.display = "none"
@@ -22,3 +30,24 @@ modal.addEventListener("click", (e) => {
     }
 })
 
+
+const continueButton = document.getElementById('continue');
+
+function waitingAnimation ()
+{
+    return new Promise ((resolve) =>
+    {
+        resolve(continueButton)
+    })
+}
+
+async function alertMsg()
+{
+    await waitingAnimation();
+    continueButton.addEventListener('animationend', () =>
+    {
+        alert('Continue to subscribe');
+    })
+}
+
+alertMsg();
